3cb9f5a1eb814fe597424c57f8643f951efc2f71
ayushavi <ayushavi.123@gmail.com>
2021-10-03 18:59:03 +0530

    Frontend Changes ADJ

--- CA_Portal/CA_Portal/settings.py
+++ CA_Portal/CA_Portal/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
