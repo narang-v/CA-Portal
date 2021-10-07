98a8f4fc81c38dee244684adcf06f5bb2156d25a
Vatan Narang <narang.iitg@gmail.com>
2021-10-07 12:06:49 +0530

    main

--- CA_Portal/home/static/home/main.js
+++ CA_Portal/home/static/home/main.js
@@ -7,25 +7,36 @@ var navbarlink5 = document.getElementById("navlink5");
 var navbarbutton = document.getElementById("join-button");
 var navbarbuttontext = document.getElementById("join-button-text");
 
-window.onscroll = function () {
-  // pageYOffset or scrollY
-  if (window.pageYOffset > 0) {
-    navbar.classList.add("scrolled");
-    navbarlink1.classList.add("scroll");
-    navbarlink2.classList.add("scroll");
-    navbarlink3.classList.add("scroll");
-    navbarlink4.classList.add("scroll");
-    navbarlink5.classList.add("scroll");
-    navbarbutton.classList.add("scroll");
-    navbarbuttontext.classList.add("scroll");
-  } else {
-    navbar.classList.remove("scrolled");
-    navbarlink1.classList.remove("scroll");
-    navbarlink2.classList.remove("scroll");
-    navbarlink3.classList.remove("scroll");
-    navbarlink4.classList.remove("scroll");
-    navbarlink5.classList.remove("scroll");
-    navbarbutton.classList.remove("scroll");
-    navbarbuttontext.classList.remove("scroll");
-  }
+window.onscroll = function() {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+        navbar.classList.add("scrolled");
+        navbarlink1.classList.add("scroll");
+        navbarlink2.classList.add("scroll");
+        navbarlink3.classList.add("scroll");
+        navbarlink4.classList.add("scroll");
+        navbarlink5.classList.add("scroll");
+        navbarbutton.classList.add("scroll");
+        navbarbuttontext.classList.add("scroll");
+    } else {
+        navbar.classList.remove("scrolled");
+        navbarlink1.classList.remove("scroll");
+        navbarlink2.classList.remove("scroll");
+        navbarlink3.classList.remove("scroll");
+        navbarlink4.classList.remove("scroll");
+        navbarlink5.classList.remove("scroll");
+        navbarbutton.classList.remove("scroll");
+        navbarbuttontext.classList.remove("scroll");
+    }
 };
+
+
+document.querySelector("#navlink5").addEventListener("click", function() {
+    document.querySelector(".popup").classList.add("active");
+    $(".backdrop").fadeTo(200, 1);
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click", function() {
+    document.querySelector(".popup").classList.remove("active");
+    $(".backdrop").fadOut(200);
+});
